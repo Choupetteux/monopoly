5d891ff32ac6788c407f2ad9d49d6c040ba25033
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-12-15 12:43:01 +0100

    Ajout du constructeur et des méthode à implémenter.

--- Monopoly/src/Case/CaseCarte.java
+++ Monopoly/src/Case/CaseCarte.java
@@ -1,8 +1,21 @@
 package Case;
 
-public class CaseCarte {
+import Joueur.Joueur;
+
+public class CaseCarte extends Case{
 
 	private int typeCarte;
+	
+	public CaseCarte(int id){
+		super();
+		this.ID = id;
+	}
+
+	@Override
+	public void jouerAction(Joueur joueur) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
