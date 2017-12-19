2afa0b817d836130cd595b1eb89886ce9c1bb73d
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-12-19 12:30:08 +0100

    Ajout d'une fenêtre de dialogue qui s'ouvre avant le début du Monopoly.

--- Monopoly/src/IHM/MonopolyApplication.java
+++ Monopoly/src/IHM/MonopolyApplication.java
@@ -2,11 +2,16 @@ package IHM;
 import java.util.ResourceBundle;
 import info.util.javafx.FXUtil;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 /**
  * Point d'entrée de l'application.
@@ -38,12 +43,23 @@ public class MonopolyApplication extends Application {
             primaryStage.setTitle("Monopoly");
             primaryStage.setScene(new Scene(root));
             primaryStage.setResizable(false);
+            Platform.setImplicitExit(false);
+
 
             // décommentez la ligne ci-dessous et remplacez "votre_icone" par
             // le nom d'une image ajoutée aux ressources de votre application
             // primaryStage.getIcons().add(new Image("votre_icone"));
 
-            primaryStage.show();
+            Stage popup = new Stage();
+            popup.show();
+            popup.setOnHidden(new EventHandler<WindowEvent>() {
+
+				@Override
+				public void handle(WindowEvent event) {
+	                primaryStage.show();
+					
+				}
+            });
 
         }
         catch (Exception e) {
